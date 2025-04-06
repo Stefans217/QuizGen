dfe183b82ee7c65da58a4f761d03e8ea71d04f06
Stef <stefanspataro@hotmail.com>
2025-04-06 15:01:01 -0700

    big config changes

--- app/client/src/services/login/loginService.tsx
+++ app/client/src/services/login/loginService.tsx
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+const backendUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001'
+    : 'https://quizgen-production.up.railway.app/';
+
 export const login = async (email: string, password: string) => {
     try {
         console.log(email, password);
-        const res = await axios.post("http://localhost:3001/api/login/login", 
+        const res = await axios.post(`${backendUrl}/api/login/login`, 
             { email, password }
         );
         localStorage.setItem("token", res.data.token);
